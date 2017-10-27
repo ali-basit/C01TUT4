d47ccff29ea9b37712a517636c1ca2e08e65dd3a
ali-basit <alirashid_1995@hotmail.com>
2017-10-27 11:21:32 -0400

    committing dummy python code

---
+++ print_name.py
@@ -0,0 +1,9 @@
+def print_name(string):
+    """Returns your name"""
+    # I can make this one line, but I need 5 lines for this tut...
+    # last line before the return, I promise
+
+    return string
+
+
+print("Ali's line")
